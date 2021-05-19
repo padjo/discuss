e077a587076b4c0507eebd56a602da2e88bfc43d
Pat Collins <padjo@mac.com>
2021-05-19 18:52:32 +0200

    added a topic migration schema

---
+++ priv/repo/migrations/20210519164736_add-topics.exs
@@ -0,0 +1,13 @@
+#mix ecto.gen.migration add-tipics
+# mix ecto.migrate
+
+defmodule :"Elixir.Discuss.Repo.Migrations.Add-topics" do
+  use Ecto.Migration
+
+  def change do
+    create table(:topics) do
+      add :title, :string
+    end
+
+  end
+end
