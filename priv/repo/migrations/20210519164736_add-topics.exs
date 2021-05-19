1f9a27163135d6bc3de363fed2ff0d71414a709d
Pat Collins <padjo@mac.com>
2021-05-19 19:39:42 +0200

    setting up router, controller for new topic

--- priv/repo/migrations/20210519164736_add-topics.exs
+++ priv/repo/migrations/20210519164736_add-topics.exs
@@ -4,7 +4,9 @@
 defmodule :"Elixir.Discuss.Repo.Migrations.Add-topics" do
   use Ecto.Migration
 
+
   def change do
+    drop_if_exists("topics")
     create table(:topics) do
       add :title, :string
     end
