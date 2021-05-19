a7ba93268093d5bf8d93efd7ce74fbc016c80239
Pat Collins <padjo@mac.com>
2021-05-19 22:08:44 +0200

    topic model and some controller added

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,4 +1,11 @@
-defmodule Discuss.TopicController do
-  def new() do
+defmodule DiscussWeb.TopicController do
+  use DiscussWeb, :controller
+
+  def new(conn, params) do
+    IO.puts("++++")
+    IO.inspect(conn)
+    IO.puts("++++")
+    IO.inspect(params)
+    IO.puts("++++")
   end
 end
